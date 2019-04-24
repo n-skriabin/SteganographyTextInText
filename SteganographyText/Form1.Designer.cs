5b5c9961b11a67bc6299c8decd6d800c62b8d7e2
n.skryabin <n.skryabin@student.csn.khai.edu>
2019-04-25 00:08:17 +0300

    Add project files.

---
+++ SteganographyText/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace SteganographyText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox_container = new System.Windows.Forms.TextBox();
+            this.textBox_result = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox_message = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(801, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Hide";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(801, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 33);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Extract";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox_container
+            // 
+            this.textBox_container.Location = new System.Drawing.Point(12, 25);
+            this.textBox_container.Multiline = true;
+            this.textBox_container.Name = "textBox_container";
+            this.textBox_container.Size = new System.Drawing.Size(785, 141);
+            this.textBox_container.TabIndex = 2;
+            // 
+            // textBox_result
+            // 
+            this.textBox_result.Location = new System.Drawing.Point(12, 348);
+            this.textBox_result.Multiline = true;
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.ReadOnly = true;
+            this.textBox_result.Size = new System.Drawing.Size(785, 141);
+            this.textBox_result.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(801, 456);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 33);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox_message
+            // 
+            this.textBox_message.Location = new System.Drawing.Point(12, 188);
+            this.textBox_message.Multiline = true;
+            this.textBox_message.Name = "textBox_message";
+            this.textBox_message.Size = new System.Drawing.Size(785, 141);
+            this.textBox_message.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Container";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Message";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 332);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(918, 498);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_message);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox_result);
+            this.Controls.Add(this.textBox_container);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "SteganographyText";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox_container;
+        private System.Windows.Forms.TextBox textBox_result;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox_message;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SteganographyText/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SteganographyText
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<char, char> AlphabetDictionary;  //<eng(1), ru(0)>
+
+        public Form1()
+        {
+            InitializeComponent();
+            AlphabetDictionary = new Dictionary<char, char>();
+
+            AlphabetDictionary.Add('a', 'а');
+            AlphabetDictionary.Add('o', 'о');
+            AlphabetDictionary.Add('p', 'р');
+            AlphabetDictionary.Add('y', 'у');
+            AlphabetDictionary.Add('c', 'с');
+            AlphabetDictionary.Add('e', 'е');
+            AlphabetDictionary.Add('x', 'х');
+
+            AlphabetDictionary.Add('A', 'А');
+            AlphabetDictionary.Add('O', 'О');
+            AlphabetDictionary.Add('P', 'Р');
+            AlphabetDictionary.Add('H', 'Н');
+            AlphabetDictionary.Add('C', 'С');
+            AlphabetDictionary.Add('E', 'Е');
+            AlphabetDictionary.Add('B', 'В');
+            AlphabetDictionary.Add('M', 'М');
+            AlphabetDictionary.Add('X', 'Х');
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox_container.Text != string.Empty) {
+
+                var stringBitArrayMessage = ToBinaryString(Encoding.Unicode, textBox_message.Text);
+                int messageCounter = 0;
+                var stringContainer = textBox_container.Text;
+                var resultCharArrayContainer = textBox_container.Text.ToCharArray();
+
+                for (int i = 0; i < stringContainer.Length - 1; i++)
+                {
+                    if (AlphabetDictionary.TryGetValue(stringContainer[i], out char value1) && stringBitArrayMessage[messageCounter] == '0')
+                    {
+                        resultCharArrayContainer[i] = value1;
+                        messageCounter++;
+                    } else if (AlphabetDictionary.TryGetValue(stringContainer[i], out char value2))
+                    {
+                        messageCounter++;
+                    }
+
+                    if (messageCounter == stringBitArrayMessage.Length)
+                    {
+                        for (int j = i; j < stringContainer.Length - 1; j++)
+                        {
+                            if (stringContainer[j] == ' ') //not spicific space
+                            {
+                                resultCharArrayContainer[j] = ' '; //spicific space (EN_SPACE, 8194)
+                                messageCounter++;
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                }
+
+                if (messageCounter < stringBitArrayMessage.Length)
+                {
+                    MessageBox.Show("Container is very small.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    textBox_result.Text = new string(resultCharArrayContainer);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var sourceMessage = textBox_container.Text;
+            string stringBitArrayMessage = string.Empty;
+
+            if (sourceMessage!= string.Empty)
+            {
+                for (int i = 0; i < sourceMessage.Length; i++)
+                {
+                    if (sourceMessage[i] == ' ')
+                    {
+                        var residual = stringBitArrayMessage.Length % 8;
+
+                        if (residual != 0)
+                        {
+                            stringBitArrayMessage = stringBitArrayMessage.Substring(0, stringBitArrayMessage.Length - residual);
+                        }
+                        break;
+                    }
+
+                    if (AlphabetDictionary.TryGetValue(sourceMessage[i], out char value))
+                    {
+                        stringBitArrayMessage += 1;
+                        continue;
+                    }
+                    var test = AlphabetDictionary.FirstOrDefault(x => x.Value == sourceMessage[i]).Key;
+                    if (AlphabetDictionary.FirstOrDefault(x => x.Value == sourceMessage[i]).Key != '\0')
+                    {
+                        stringBitArrayMessage += 0;
+                    }
+                }
+
+                var resultString = BinaryToString(stringBitArrayMessage);
+                textBox_result.Text = resultString;
+            }
+            else
+            {
+                MessageBox.Show("Container is empty.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        static string ToBinaryString(Encoding encoding, string text)
+        {
+            return string.Join("", encoding.GetBytes(text).Select(n => Convert.ToString(n, 2).PadLeft(8, '0')));
+        }
+
+        public static string BinaryToString(string data)
+        {
+            List<Byte> byteList = new List<Byte>();
+
+            for (int i = 0; i < data.Length; i += 8)
+            {
+                byteList.Add(Convert.ToByte(data.Substring(i, 8), 2));
+            }
+            var result = Encoding.Unicode.GetString(byteList.ToArray());
+            return result;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }        
+    }
+}
